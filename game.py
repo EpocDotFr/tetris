bc36b47080af5dd455256ffde56f80531575f885
EpocDotFr <contact@epoc.fr>
2017-09-04 15:02:02 +0200

    More refactoring

--- game.py
+++ game.py
@@ -13,6 +13,9 @@ class Game:
 
         pygame.display.set_caption('Tetris')
 
+        self._init_new_game()
+
+    def _init_new_game(self):
         self.well = {}
 
         for x in range(0, settings.COLS):
@@ -25,34 +28,28 @@ class Game:
 
         pygame.time.set_timer(settings.TETRIMINOS_FALLING_EVENT, settings.TETRIMINOS_FALLING_INTERVAL)
 
-    def update(self):
-        if not self.current_tetrimino:
-            self.current_tetrimino = getattr(tetriminos, random.choice(tetriminos.__all__))()
+    def _set_current_tetrimino(self):
+        if self.current_tetrimino:
+            return
 
-            self.current_tetrimino.rect.left = (settings.COLS // 2) * ((self.current_tetrimino.cols // 2) * settings.BLOCKS_SIDE_SIZE)
-            self.current_tetrimino.rect.top = 0
+        self.current_tetrimino = getattr(tetriminos, random.choice(tetriminos.__all__))()
+
+        self.current_tetrimino.rect.left = (settings.COLS // 2) * ((self.current_tetrimino.cols // 2) * settings.BLOCKS_SIDE_SIZE)
+        self.current_tetrimino.rect.top = 0
+
+    def update(self):
+        self._set_current_tetrimino()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                self._event_quit()
             elif event.type == settings.TETRIMINOS_FALLING_EVENT:
-                if self.current_tetrimino.rect.bottom + settings.BLOCKS_SIDE_SIZE > self.window_rect.h:
-                    self.current_tetrimino = None
-                else:
-                    self.current_tetrimino.rect.bottom += settings.BLOCKS_SIDE_SIZE
+                self._event_falling_tetrimino()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                elif event.key == pygame.K_LEFT and self.current_tetrimino.rect.left - settings.BLOCKS_SIDE_SIZE >= 0:
-                    self.current_tetrimino.rect.left -= settings.BLOCKS_SIDE_SIZE
-                elif event.key == pygame.K_RIGHT and self.current_tetrimino.rect.right + settings.BLOCKS_SIDE_SIZE <= self.window_rect.w:
-                    self.current_tetrimino.rect.left += settings.BLOCKS_SIDE_SIZE
-                elif event.key == pygame.K_DOWN:
-                    pass
-                elif event.key == pygame.K_UP:
-                    self.current_tetrimino.rotate()
+                    self._event_quit()
+                else:
+                    self._event_game_key(event)
 
         self.window.fill((255, 255, 255))
 
@@ -66,4 +63,31 @@ class Game:
                     pygame.draw.line(self.window, (225, 225, 225), (0, y * settings.BLOCKS_SIDE_SIZE), (self.window_rect.w, y * settings.BLOCKS_SIDE_SIZE))
 
         pygame.display.update()
+
         self.clock.tick(settings.FPS)
+
+    # --------------------------------------------------------------------------
+    # Events handlers
+
+    def _event_quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def _event_falling_tetrimino(self):
+        if self.current_tetrimino.rect.bottom + settings.BLOCKS_SIDE_SIZE > self.window_rect.h:
+            self.current_tetrimino = None
+        else:
+            self.current_tetrimino.rect.bottom += settings.BLOCKS_SIDE_SIZE
+
+    def _event_game_key(self, event):
+        if event.key == pygame.K_LEFT and self.current_tetrimino.rect.left - settings.BLOCKS_SIDE_SIZE >= 0:
+            self.current_tetrimino.rect.left -= settings.BLOCKS_SIDE_SIZE
+        elif event.key == pygame.K_RIGHT and self.current_tetrimino.rect.right + settings.BLOCKS_SIDE_SIZE <= self.window_rect.w:
+            self.current_tetrimino.rect.left += settings.BLOCKS_SIDE_SIZE
+        elif event.key == pygame.K_DOWN:
+            pass
+        elif event.key == pygame.K_UP:
+            self.current_tetrimino.rotate()
+
+    # --------------------------------------------------------------------------
+    # Display handlers
